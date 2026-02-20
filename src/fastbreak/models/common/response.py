208e4c0a746ae360f710bbecba402e15d3d3655a
Reid Hochstedler <reidhoch@gmail.com>
2026-02-20 11:23:15 -0500

    Sync API Endpoints schema

--- src/fastbreak/models/common/response.py
+++ src/fastbreak/models/common/response.py
@@ -6,6 +6,17 @@ from pydantic import BaseModel, ConfigDict, model_validator
 
 from fastbreak.logging import logger
 
+# Standard NBA API wrapper fields that are handled by transformation validators
+# and should not trigger unknown field warnings
+_API_WRAPPER_FIELDS: frozenset[str] = frozenset(
+    {
+        "resource",  # Endpoint name
+        "parameters",  # Request parameters echoed back
+        "resultSet",  # Single result set (tabular format)
+        "resultSets",  # Multiple result sets (tabular format)
+    }
+)
+
 
 class FrozenResponse(BaseModel):
     """Base class for API response models.
@@ -28,8 +39,15 @@ class FrozenResponse(BaseModel):
         """Log a warning if the API returns fields we don't have defined."""
         if not isinstance(data, dict):
             return data
-        model_fields = set(cls.model_fields.keys())
-        extra_fields = set(data.keys()) - model_fields
+
+        # Collect both field names and their aliases
+        known_keys: set[str] = set(_API_WRAPPER_FIELDS)
+        for field_name, field_info in cls.model_fields.items():
+            known_keys.add(field_name)
+            if field_info.alias:
+                known_keys.add(field_info.alias)
+
+        extra_fields = set(data.keys()) - known_keys
         if extra_fields:
             logger.warning(
                 "unknown_fields_received",
